6e30263eb63e0243e95e03aca38e34e7062342c4
v0-e <xyz.vieira@gmail.com>
2025-01-23 18:56:45 +0000

    Add SOD verification to `bin/reader`

--- src/bin/reader.rs
+++ src/bin/reader.rs
@@ -2,17 +2,38 @@
 
 use {
     anyhow::{anyhow, Context, Result},
+    der::Decode,
     icao_9303::{
-        asn1::emrtd::EfSod,
+        asn1::emrtd::{pki::MasterList, EfSod},
+        crypto::{TrustPolicy, TrustStore},
         emrtd::{Emrtd, Error, FileId},
         ensure_err,
         iso7816::StatusWord,
         nfc::connect_reader,
     },
-    std::env,
+    std::{env, fs},
 };
 
 // https://github.com/RfidResearchGroup/proxmark3/issues/1117
+
+fn load_truststore() -> Result<TrustStore> {
+    // Initialize Trust Store, accepting some CSCA deviations
+    let mut store = TrustStore::new(TrustPolicy::Relaxed);
+
+    print!("Setting up trust store with:");
+    for entry in fs::read_dir("mls")? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            print!(" {}", path.file_name().unwrap().to_string_lossy());
+            let ml = MasterList::from_der(&fs::read(&path)?)?;
+            store.add_master_list(&ml)?;
+        }
+    }
+    println!();
+
+    Ok(store)
+}
 
 fn main() -> Result<()> {
     let mut rng = rand::thread_rng();
@@ -33,14 +54,24 @@ fn main() -> Result<()> {
         .context("Error during Basic Access Control.")?;
     eprintln!("Basic Access Control successful.");
 
-    // let ef_sod = card.read_cached::<EfSod>()?;
+    // Verify signature
+    let mut store = load_truststore()?;
+    let ef_sod = card.read_cached::<EfSod>()?;
+    ef_sod.verify_signature(&mut store)?;
     // println!("DOCUMENT HASH = 0x{}", hex::encode(ef_sod.document_hash()));
 
     // Should be secured now!
     // Let's read some files.
     for file_id in FileId::iter() {
         match card.read_file_cached(file_id) {
-            Ok(Some(data)) => println!("{}: {}", file_id, hex::encode(data)),
+            Ok(Some(data)) => {
+                let in_sod = ef_sod.contains_file_bin(&data, file_id).is_ok();
+                println!(
+                    "{} (SOD-authenticated: {in_sod:?}): {}",
+                    file_id,
+                    hex::encode(data)
+                );
+            }
             Ok(None) => println!("{}: Not Found", file_id),
             Err(Error::ErrorResponse(StatusWord::ACCESS_DENIED)) => {
                 println!("{}: Access Denied", file_id)
@@ -48,9 +79,6 @@ fn main() -> Result<()> {
             Err(e) => eprintln!("{}: {}", file_id, e),
         }
     }
-
-    // TODO: Verify SOD.
-    // https://github.com/worldcoin/nfc-uniqueness-service/blob/d907d9ef33826034665592685c1e24d25bdb1259/src/routes/v1/mod.rs#L102
 
     // Active Authentication with fixed nonce
     // // ICAO 9303-11 section 6.1
