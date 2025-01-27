d86df589eb1220feb52e5c620d497bdee23072da
v0-e <xyz.vieira@gmail.com>
2025-01-27 12:15:51 +0000

    Add `Emrtd::commands()` dispatcher

--- src/emrtd/commands.rs
+++ src/emrtd/commands.rs
@@ -1,7 +1,12 @@
 use {
+<<<<<<< HEAD
     super::{iso7816::Apdu, Emrtd},
     anyhow::{ensure, Result},
     bytes::Buf,
+=======
+    super::Emrtd,
+    anyhow::{anyhow, ensure, Result},
+>>>>>>> a207199 (Add `Emrtd::commands()` dispatcher)
 };
 
 pub struct Commands<'a> {
